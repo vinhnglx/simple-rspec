527519996a432d0208ae10d02c2b2e59693fb6de
Nhu Huynh <nhu.huynh@asnet.com.vn>
2013-07-21 21:22:28 +0700

    initialize code skeloton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleRspec::Application.config.secret_key_base = 'a1c81494a1cf8d4d260fb6f4cfe4a8c94a0154cb49f8402ebb63a758af1bd63ddd50854ae6a8020d668a0c2e8543b87baaff552d2139f06892b54abfeb25f94b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleRspec::Application.config.session_store :cookie_store, key: '_simple-rspec_session'
